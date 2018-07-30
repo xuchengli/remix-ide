9f5720281ba0aa34e9897f19646362df918a5fc0
xuchengli <lixucheng@aliyun.com>
2018-07-30 10:07:22 +0800

    update API server url

--- src/app/tabs/run-tab.js
+++ src/app/tabs/run-tab.js
@@ -160,7 +160,7 @@ function fillEnvironmentList (appAPI, opts, el, event) {
   const selectExEnv = el.querySelector('#selectExEnvOptions')
   if (apiToken) {
     request({
-      url: 'https://api-baas.zhigui.com/v1/public-chain/list',
+      url: 'http://120.92.100.153/v1/public-chain/list',
       headers: {
         'Authorization': `Bearer ${apiToken}`
       },
